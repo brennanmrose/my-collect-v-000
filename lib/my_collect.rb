e034774893aab1903743cd4e95d09a285763080c
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-21 02:12:13 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,3 +2,15 @@ def my_collect(array)
 end 
 
 
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
